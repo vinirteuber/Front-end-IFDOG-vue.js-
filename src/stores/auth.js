564eee71e18031619bd9ed45fd6e6ddd856a332c
LocatelliBryann <bryannlocatelli@gmail.com>
2022-11-17 09:37:57 -0300

    Foto Usuário

---
+++ src/stores/auth.js
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+export const useAuthStore = defineStore({
+  id: "auth",
+  state: () => ({
+    access: "",
+    username: "",
+    id: 0,
+    email: "",
+    first_name: "",
+  }),
+  actions: {
+    async login(user) {
+      const { data } = await axios.post(
+        "http://localhost:8000/token/",
+        user
+      );
+      this.access = data.access;
+      this.username = data.username;
+      this.id = data.id;
+      this.email = data.email;
+      this.first_name = data.first_name;
+    },
+  },
+});
